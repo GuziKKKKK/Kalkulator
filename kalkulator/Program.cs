d6d2fa115d6f0743fbd8862e376fb8211096c4da
studentwsb <studentwsb@STUDENTWSB>
2019-09-22 15:39:29 +0200

    Add multiplication to menu

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -12,6 +12,7 @@ namespace kalkulator
                 Console.WriteLine("0. Exit");
                 Console.WriteLine("1. Dodawanie");
                 Console.WriteLine("2. Odejmowanie");
+                Console.WriteLine("3. Mnozenie");
 
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
